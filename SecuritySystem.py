d90963ca915752fbb3251d3a3ab183098a5cbe77
shearocke <rockeshea72@gmail.com>
2019-10-10 14:10:26 +1000

    test1

---
+++ SecuritySystem.py
@@ -0,0 +1,25 @@
+import cv2
+from datetime import datetime
+
+cam = cv2.VideoCapture(0)  # select camera feed
+cam.set(cv2.CAP_PROP_FPS, 60)  # set fps
+cam.set(3, 1280)  # set x resolution     1920
+cam.set(4, 720)  # set y resolution 720p 1080
+font = cv2.FONT_HERSHEY_TRIPLEX
+
+while True:
+    re, img = cam.read()  # setting the camera feed to return an image
+
+    img = cv2.flip(img, 1)  # flip to mirror image vertically
+    #           video      string             position    font size   colour  stroke
+    cv2.putText(img, "You are being recorded", (250, 400), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText()
+
+    cv2.imshow('Shea Video', img)  # displays camera feed window
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:  # wait for ECS key to end loop
+        break
+
+cam.release()
+cv2.destroyAllWindows()
