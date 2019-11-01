31775e84ad5cc0c07ea9cca94bc91db60441d30f
shearocke <rockeshea72@gmail.com>
2019-11-01 14:43:59 +1000

    school laptop 1/11/19

---
+++ myPlot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+ylist = [1, 12, 32, 91, 28, 4, 20, 180, 97, 77]
+xlist = ['jorge', 'Rob', 'Jerry', 'Duke', 'Clary', 'Rodger', 'Corey', 'Gary', 'Patrick', 'Sandy']
+
+plt.xlabel('This is the X label')
+plt.ylabel('This is the Y label')
+
+plt.title('This is the title!')
+
+plt.plot(xlist, ylist)
+
+plt.show()
