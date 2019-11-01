0330b9afca197fa9f3bb6cdf513c07b3d4aa4dce
shearocke <rockeshea72@gmail.com>
2019-11-01 14:17:29 +1000

    school laptop 1/11/19

--- sqlBearsic.py
+++ sqlBearsic.py
@@ -12,7 +12,9 @@ conn = sqlite3.connect(database)
 
 #record = conn.execute('SELECT Name, LifeExpectancy FROM Country WHERE LifeExpectancy > 80 ORDER BY LifeExpectancy;')
 #record = conn.execute("SELECT Name, HeadOfState, GovernmentForm FROM Country WHERE GovernmentForm = 'Republic' ORDER BY Name")
-record = conn.execute("SELECT Name, GNP, Population FROM Country WHERE GNP > 10000 ORDER BY GNP")
+#record = conn.execute("SELECT Name, GNP, Population FROM Country WHERE GNP > 10000 ORDER BY GNP")
+#record = conn.execute("SELECT Name, Population, SurfaceArea FROM Country WHERE Population > 1000000000 ORDER BY Population")
+record = conn.execute("SELECT Name, Population, SurfaceArea FROM Country WHERE SurfaceArea < 10000000 AND Population > 1000000000")
 for row in record:
     print(row)
     #tts.say(row)
